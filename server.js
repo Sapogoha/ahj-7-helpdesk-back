b52157566e61c6c01e0cfa8c28d6e15542704df6
Aleksey Sokolov <sokolov.alexey@me.com>
2022-05-11 22:29:32 +0300

    server works

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const http = require('http');
 const path = require('path');
-// const fs = require('fs');
 const Koa = require('koa');
+const Router = require('koa-router');
 const koaBody = require('koa-body');
 const koaStatic = require('koa-static');
 const { v4: uuid } = require('uuid');
 const cors = require('@koa/cors');
-
+const router = new Router();
+const app = new Koa();
+const port = process.env.PORT || 7070;
+const public = path.join(__dirname, '/public');
 const tickets = [
   {
     id: '1',
-    name: 'Sample task 1',
-    description: 'Additional info 1',
+    name: 'Задание 1',
+    description: 'Описание задания 1',
     status: true,
     created: 1651597184204,
   },
   {
     id: '2',
-    name: 'Sample task 2',
-    description: 'Additional info 2',
+    name: 'Задание 2',
+    description: 'Описание задания 2',
     status: false,
     created: 1651597184204,
   },
   {
     id: uuid(),
-    name: 'Sample task 3',
-    description: 'Additional info 3',
+    name: 'Задание 3',
+    description: 'Описание задания 3',
     status: false,
     created: 1651597184204,
   },
@@ -44,16 +47,9 @@ function createNewTicket(params) {
   }
 }
 
-const app = new Koa();
-const port = process.env.PORT || 7070;
-
-const public = path.join(__dirname, '/public');
 app.use(koaStatic(public));
 app.use(
   cors({
-    origin: '*',
-    credentials: true,
-    'Access-Control-Allow-Origin': true,
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
 );
@@ -67,67 +63,51 @@ app.use(
   })
 );
 
-app.use(async (ctx) => {
-  const requestMethod = ctx.request.method;
-  const { method, id } = ctx.request.query;
+router.get('/tickets', async (ctx) => {
+  const { method } = ctx.request.query;
+  if (method === 'allTickets') {
+    ctx.response.body = tickets;
+  }
 
-  if (requestMethod === 'GET') {
-    switch (method) {
-      case 'allTickets':
-        ctx.response.body = tickets;
-        return;
-      case 'ticketById':
-        console.log(tickets);
-        console.log(id);
-        ctx.response.body = tickets.find((t) => t.id === id);
-        return;
-      default:
-        ctx.response.status = 404;
-        return;
-    }
-  } else if (requestMethod === 'POST') {
-    switch (method) {
-      case 'createTicket':
-        ctx.response.body = createNewTicket(ctx.request.body);
-        return;
-      default:
-        ctx.response.status = 404;
-        return;
-    }
-  } else if (requestMethod === 'PUT') {
-    const ticket = this.tickets.find((t) => t.id === id);
-
-    switch (method) {
-      case 'editById':
-        const { name, description } = ctx.request.body;
-
-        ticket.name = name;
-        ticket.description = description;
-        ctx.response.status = 204;
-        return;
-      case 'changeStatus':
-        ticket.status ? (ticket.status = false) : (ticket.status = true);
-        ctx.response.status = 204;
-        return;
-      default:
-        ctx.response.status = 404;
-        return;
-    }
-  } else if (requestMethod === 'DELETE') {
-    switch (method) {
-      case 'remove':
-        const tickett = this.tickets.find((t) => t.id === id);
-        tickets.splice(tickett, 1);
-
-        ctx.response.status = 204;
-        return;
-
-      default:
-        ctx.response.status = 404;
-        return;
-    }
+  if (method === 'ticketById') {
+    const { id } = ctx.request.query;
+    ctx.response.body = tickets.find((t) => t.id === id);
   }
 });
 
+router.post('/tickets', async (ctx) => {
+  ctx.response.body = ctx.request.body;
+  const { method } = ctx.request.query;
+  if (method === 'createTicket') {
+    createNewTicket(ctx.request.body);
+    ctx.response.status = 204;
+  }
+});
+
+router.put('/tickets', async (ctx) => {
+  const { id } = ctx.request.body;
+  const { method } = ctx.request.query;
+  const ticket = tickets.find((t) => t.id === id);
+
+  if (method === 'editById') {
+    const { name, description } = ctx.request.body;
+    ticket.name = name;
+    ticket.description = description;
+  } else if (method === 'changeStatus') {
+    console.log(ticket.status);
+    ticket.status ? (ticket.status = false) : (ticket.status = true);
+  }
+
+  ctx.response.status = 204;
+});
+
+router.delete('/tickets/remove/:id', async (ctx) => {
+  const id = ctx.params.id;
+  const ticketIndex = tickets.findIndex((ticket) => ticket.id === id);
+  tickets.splice(ticketIndex, 1);
+  ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 const server = http.createServer(app.callback());
 server.listen(port, () => console.log('Server started'));
